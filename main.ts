8ad24fb5628552d12e5d60b543c86b273dced5e4
Leonardo Lima <leolima.str@gmail.com>
2022-11-01 11:01:51 -0300

    refactor: isolating server application

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-console.log('main script...d 2')
+import { Server } from './server/server'
+
+const server = new Server()
+
+server.bootstrap().then(server => {
+    console.log("Server is listeninng on:", server.application.address())
+}).catch(error => {
+    console.log('Server failed to start')
+    console.error(error)
+    process.exit(1)
+})
