42639d00ade383450b0602f99ac89f4fbc36847d
Leonardo Lima <leolima.str@gmail.com>
2022-11-01 17:31:38 -0300

    chore: configuring database connection

--- common/environment.ts
+++ common/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    server: { port: process.env.SERVER_PORT || 4000 }
+    server: { port: process.env.SERVER_PORT || 4000 },
+    db: { url: process.env.DB_URL || 'mongodb://localhost/auth-pro' }
 }
