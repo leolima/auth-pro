e84333d68f885686297207211bf205acd9715811
Leonardo Lima <leolima.str@gmail.com>
2022-11-01 17:33:24 -0300

    feat: writing the routes for users

--- users/users.router.ts
+++ users/users.router.ts
@@ -4,21 +4,90 @@ import { User } from './users.model'
 
 class UsersRouter extends Router {
     applyRoutes(application: restify.Server) {
+        /**
+         * Get All users
+         */
         application.get('/users', (req, resp, next) => {
-            User.findAll().then(users => {
-                resp.json(users)
-                return next()
-            })
+            User.find()
+                .then(users => {
+                    resp.json(users)
+                    return next()
+                })
         })
 
+        /**
+         * Find User
+         */
         application.get('/users/:id', (req, resp, next) => {
-            User.findById(req.params.id).then(user => {
-                if (user) {
+            User.findById(req.params.id || undefined)
+                .then(user => {
+                    if (user) {
+                        resp.json(user)
+                        return next()
+                    }
+                    resp.send(404)
+                    return next()
+                })
+        })
+
+        /**
+         * Add User
+         */
+        application.post('/users', async (req, resp, next) => {
+            let user = new User(req.body)
+            user.save()
+                .then(user => {
+                    user.password = undefined
                     resp.json(user)
                     return next()
-                }
+                })
+        })
 
-                resp.send(404)
+        /**
+         * Replace User
+         */
+        application.put('/users/:id', (req, resp, next) => {
+            User.replaceOne({ _id: req.params.id || undefined }, req.body)
+                .then(result => {
+                    if (result.matchedCount) {
+                        User.findById(req.params.id).then(user => {
+                            resp.json(user)
+                            return next()
+                        })
+                    } else {
+                        resp.send(404)
+                        return next()
+                    }
+                })
+        })
+
+        /**
+         * Update user
+         */
+        application.patch('/users/:id', (req, resp, next) => {
+            const options = { new: true }
+            User.findByIdAndUpdate(req.params.id || undefined, req.body, options)
+                .then(user => {
+                    if (user) {
+                        resp.json(user)
+                        return next()
+                    } else {
+                        resp.send(404)
+                        return next()
+                    }
+                })
+        })
+
+        /**
+        * Delete user
+        */
+        application.del('/users/:id', (req, resp, next) => {
+            User.deleteOne({ _id: req.params.id || undefined }).then(result => {
+                if (result.deletedCount) {
+                    resp.send(204)
+                } else {
+                    resp.send(404)
+                }
                 return next()
             })
         })
