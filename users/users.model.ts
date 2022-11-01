6d70f06d2ee19db9addc35b4cfda31a2ef1a8d39
Leonardo Lima <leolima.str@gmail.com>
2022-11-01 17:32:32 -0300

    feat: creating user schema

--- users/users.model.ts
+++ users/users.model.ts
@@ -1,20 +1,28 @@
-const users = [
-    { id: '1', name: 'Peter Parker', email: 'peter@mavel.com' },
-    { id: '2', name: 'Bruce Wayne', email: 'bruce@dc.com' }
-]
+import * as mongoose from 'mongoose'
 
-export class User {
-    static findAll(): Promise<any[]> {
-        return Promise.resolve(users)
-    }
-    static findById(id: string): Promise<any[]> {
-        return new Promise((resolve) => {
-            const filtered = users.filter(u => u.id === id)
-            let user = undefined
-            if (filtered.length > 0) {
-                user = filtered[0]
-            }
-            resolve(user)
-        })
-    }
+// interfaces are not exported in typescript
+export interface User extends mongoose.Document {
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string
 }
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+        select: false
+    }
+})
+
+export const User = mongoose.model<User>('User', userSchema)
