c001bcce67156ab7bc852ce899d522177d8287ea
Leonardo Lima <leolima.str@gmail.com>
2022-11-04 11:27:05 -0300

    feat: add support to pagination and filters

--- common/model-router.ts
+++ common/model-router.ts
@@ -4,9 +4,40 @@ import { Router } from './router'
 
 
 export abstract class ModelRouter<D extends mongoose.Document> extends Router {
+    basePath: string
+    pageSize: number = 2
+
     constructor(protected model: mongoose.Model<D>) {
         super()
+        this.basePath = `/${ model.collection.name }`
     }
+
+    envelope(document: any): any {
+        let resource = Object.assign({ _links: {} }, document.toJSON())
+        resource._links.self = `${ this.basePath }/${ resource._id }`
+
+        return resource
+    }
+
+    envelopeAll(documents: any[], options: any = {}): any {
+        const resource: any = {
+            _links: {
+                self: `${ options.url }`,
+            },
+            items: documents,
+        }
+        if (options.page && options.count && options.pageSize) {
+            if (options.page > 1) {
+                resource._links.previous = `${ this.basePath }?_page=${ options.page - 1 }`
+            }
+            const remaining = options.count - options.page * options.pageSize
+            if (remaining > 0) {
+                resource._links.next = `${ this.basePath }?_page=${ options.page + 1 }`
+            }
+        }
+        return resource
+    }
+
 
     validateId = (req, resp, next) => {
         if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
@@ -17,11 +48,30 @@ export abstract class ModelRouter<D extends mongoose.Document> extends Router {
     }
 
     /**
-     * Get All documents
+     * Get All documents with filters and pagination
      */
     findAll = (req, resp, next) => {
-        this.model.find()
-            .then(this.renderAll(resp, next))
+        let page = parseInt(req.query.page || 1)
+        page = page > 0 ? page : 1
+        const skip = (page - 1) * this.pageSize
+        this.model
+            .countDocuments(req.query || null)
+            .exec()
+            .then((count) => {
+                this.model
+                    .find(req.query || null)
+                    .skip(skip)
+                    .limit(this.pageSize)
+                    .then(
+                        this.renderAll(resp, next, {
+                            page,
+                            count,
+                            pageSize: this.pageSize,
+                            url: req.url,
+                        })
+                    )
+            }
+            )
             .catch(next)
     }
 
